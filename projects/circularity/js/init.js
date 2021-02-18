e46a5375af9b8a7b76304c4059818a7fe5fda15c
EmilyNiemand <eminiemand@gmail.com>
2021-02-18 15:48:24 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -78,7 +78,7 @@ var init = function (window) {
             if ( circle.x < 0) {
                 circle.x = 0;
             }
-            if ( circle.y > canvas.width ) {
+            if ( circle.y > canvas.height ) {
                 circle.y = 0;
             }
             if ( circle.y < 0){
