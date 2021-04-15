435fdc41842d58b6d98a2562bbb544fcec2a2cfd
EmilyNiemand <eminiemand@gmail.com>
2021-04-15 14:49:34 +0000

    add runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -109,9 +109,9 @@ var level01 = function (window) {
         
         
          function createReward(x,y){
-              var hitZoneSize = .5;
+              var rewardHitZoneSize = .5;
              var reward = game.createGameItem('reward',25);
-            var pinkSquare = draw.rect(50,50,'pink');
+            var pinkSquare = draw.rect(25,25,'pink');
             pinkSquare.x = x;
             pinkSquare.y = x;
             reward.addChild(pinkSquare);
